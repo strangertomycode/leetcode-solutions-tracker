c5a44e9197121a1416c6ea96914a1a0c73f97385
Sreeram P <114813900+strangertomycode@users.noreply.github.com>
2026-06-23 13:50:18 +0530

    feat(easy/string): solve #20 Valid Parentheses

---
+++ 20. Valid Parentheses/valid_parentheses.py
@@ -0,0 +1,30 @@
+"""
+Problem : 20. Valid Parentheses
+Difficulty: Easy
+Topics   : String, Stack
+Runtime  : 0
+Memory   : 19380000
+Language : python3
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parantheses_map = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "[",
+        }
+
+        stack = []
+
+        for p in s:
+            if p in parantheses_map:
+                if stack and stack[-1] == parantheses_map[p]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(p)
+        
+        return len(stack) == 0
+        
