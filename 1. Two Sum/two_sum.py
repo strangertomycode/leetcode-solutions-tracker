2674311cec5ed0d2e60dbe1c92de7c186e751d72
Sreeram P <114813900+strangertomycode@users.noreply.github.com>
2026-06-23 13:50:25 +0530

    feat(easy/array): solve #1 Two Sum

---
+++ 1. Two Sum/two_sum.py
@@ -0,0 +1,20 @@
+"""
+Problem : 1. Two Sum
+Difficulty: Easy
+Topics   : Array, Hash Table
+Runtime  : 0
+Memory   : 20380000
+Language : python3
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        n = len(nums)
+
+        for i in range(n):
+            rem = target - nums[i]
+            if rem in num_map:
+                return [num_map[rem], i]
+            else:
+                num_map[nums[i]] = i
