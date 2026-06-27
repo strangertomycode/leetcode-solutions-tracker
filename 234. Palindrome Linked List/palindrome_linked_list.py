13986eb0c3d86b2fe79f60adb204297acb1c47bf
Sreeram P <114813900+strangertomycode@users.noreply.github.com>
2026-06-27 22:53:03 +0530

    Solve #234 Palindrome Linked List [easy/linked-list]

--- 234. Palindrome Linked List/palindrome_linked_list.py
+++ 234. Palindrome Linked List/palindrome_linked_list.py
@@ -2,8 +2,8 @@
 Problem : 234. Palindrome Linked List
 Difficulty: Easy
 Topics   : Linked List, Two Pointers, Stack, Recursion
-Runtime  : 17
-Memory   : 53364000
+Runtime  : 39
+Memory   : 42720000
 Language : python3
 """
 
@@ -14,11 +14,39 @@ Language : python3
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        curr = head
-        linked_list = []
+        # curr = head
+        # linked_list = []
+
+        # while curr:
+        #     linked_list.append(curr.val)
+        #     curr = curr.next
+
+        # return linked_list == linked_list[::-1]
+
+        s, f = head, head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+        
+        prev = None
+        curr = s
 
         while curr:
-            linked_list.append(curr.val)
-            curr = curr.next
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
 
-        return linked_list == linked_list[::-1]
+        left = head
+        right = prev
+
+        while right:
+            if right.val != left.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+
