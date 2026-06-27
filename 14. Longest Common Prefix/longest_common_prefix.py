ab4aa3849577846b9d5e3ea3c34e0077d08eb2f3
Sreeram P <114813900+strangertomycode@users.noreply.github.com>
2026-06-27 16:11:38 +0530

    Solve #14 Longest Common Prefix [easy/array]

---
+++ 14. Longest Common Prefix/longest_common_prefix.py
@@ -0,0 +1,31 @@
+"""
+Problem : 14. Longest Common Prefix
+Difficulty: Easy
+Topics   : Array, String, Trie
+Runtime  : 0
+Memory   : 19292000
+Language : python3
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        # Take the first word as our reference.
+        first = strs[0]
+
+        for i in range(len(first)):
+
+            # Compare the current character with every other word.
+            for word in strs[1:]:
+
+                if i == len(word) or first[i] != word[i]:
+
+                    # Return everything before this position.
+                    #
+                    # Example:
+                    # first = "flower"
+                    # i = 2
+                    #
+                    # first[:2] -> "fl"
+                    return first[:i]
+        return first
